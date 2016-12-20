69249dcbeef1bd0eca82095992386e6771bb7112
jd <NikeLambert@gmail.com>
2016-12-20 15:37:21 +0300

    Создан первый маршрут

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 __author__ = "Evgeny Goncharov"
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
+
+app.run(host="192.168.1.102", port=5000, debug=True)
