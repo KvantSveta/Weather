db48255af833755f0f46a23ae16a5d7f2ccd7eb4
jd <NikeLambert@gmail.com>
2016-12-26 18:09:57 +0300

    Модуль с тестами

---
+++ test.py
@@ -0,0 +1,38 @@
+import unittest2 as unittest
+
+class TestGeneratorSpeed(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        from random import randint
+        self.a = []
+        self.b = []
+        minus = chr(8722)
+        for _ in range(10**7):
+            i = randint(-100, 100)
+            if i < 0:
+                i = str(i)
+                i = i.replace('-', minus)
+            else:
+                i = str(i)
+            self.a.append(i)
+            self.b.append(i)
+
+    def test_1(self):
+        import time
+
+        s = time.time()
+        [c.replace(chr(8722), '-') for c in self.a]
+        print('test 1:', time.time() - s)
+
+    def test_2(self):
+        import time
+
+        s = time.time()
+        minus = chr(8722)
+        [c.replace(minus, '-') for c in self.b]
+        print('test 2:', time.time() - s)
+
+    @classmethod
+    def tearDownClass(self):
+        del self.a
+        del self.b
