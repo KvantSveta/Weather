c73437641b1287d4b11b9fe191d5fa1469c1c7fc
jd <NikeLambert@gmail.com>
2016-12-26 22:34:26 +0300

    Модуль получает информацию о погоде на весь день

--- weather.py
+++ weather.py
@@ -1,22 +1,23 @@
-# httpbin.org сайт для проверки корректности запросов
-# page = 'http://httpbin.org'
+__author__ = "Evgeny Goncharov"
 
-import urllib.request
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 page = "https://www.gismeteo.ru/weather-stavropol-5141/"
 
-response = urllib.request.urlopen(page)
-response = response.read()#.decode("utf-8")
-
-from bs4 import BeautifulSoup
+response = urlopen(page).read() #.decode("utf-8")
 
 soup = BeautifulSoup(response, 'lxml')
+# выбор всех значений о погоде
+# [s.text for s in soup.find_all('div', 'weather_item')]
 
-minus = chr(8722)
-
-array = [int(s.text.replace(minus, '-')) for s in soup.find_all('span', 'js_value val_to_convert')]
-
-middle = int(len(array) / 2)
-# массивы с температурой и давлением за сутки
-temperature, pressure = array[:middle], array[middle:]
-
+# температура, градус Цельсия
+temperature = [s.text for s in soup.find_all('div', 'weather_item js_temp_graph')]
+# скорость и направление ветра, м/с
+wind = [s.text for s in soup.find_all('div', 'wind_value js_meas_container')]
+# давление, мм рт.ст.
+pressure = [s.text for s in soup.find_all('div', 'weather_item js_pressure_graph')]
+# влажность, %
+humidity = [s.text for s in soup.find_all('div', 'humidity_value')]
+# осадки, мм
+rainfall = [s.text.strip() for s in soup.find_all('div', 'weather_item')][-9:]
