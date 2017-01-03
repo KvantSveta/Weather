d97f4f0db234f4c870cd1188427f919ffb69ddf0
jd <NikeLambert@gmail.com>
2017-01-03 19:39:18 +0300

    Написан класс для получения и сохранения погоды за день

--- weather.py
+++ weather.py
@@ -1,28 +1,62 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import time
 
 
 __author__ = "Evgeny Goncharov"
 
-page = "https://www.gismeteo.ru/weather-stavropol-5141/"
 
-response = urlopen(page).read()  # .decode("utf-8")
+class Weather():
+    def __init__(self):
+        self._temperature = None
+        self._wind = None
+        self._pressure = None
+        self._humidity = None
+        self._precipitation = None
 
-soup = BeautifulSoup(response, 'lxml')
-# выбор всех значений о погоде
-# [s.text for s in soup.find_all('div', 'weather_item')]
+    def set(self):
+        page = "https://www.gismeteo.ru/weather-stavropol-5141/"
 
-# температура, градус Цельсия
-temperature = [
-    s.text for s in soup.find_all('div', 'weather_item js_temp_graph')
-]
-# скорость и направление ветра, м/с
-wind = [s.text for s in soup.find_all('div', 'wind_value js_meas_container')]
-# давление, мм рт.ст.
-pressure = [
-    s.text for s in soup.find_all('div', 'weather_item js_pressure_graph')
-]
-# влажность, %
-humidity = [s.text for s in soup.find_all('div', 'humidity_value')]
-# осадки, мм
-rainfall = [s.text.strip() for s in soup.find_all('div', 'weather_item')][-9:]
+        response = urlopen(page).read()
+
+        s = BeautifulSoup(response, 'lxml')
+
+        # температура, градус Цельсия
+        _s = s.find('div', 'templine_inner')
+        self._temperature = [
+            t.text for t in _s.find_all('span', 'js_value val_to_convert')
+        ]
+
+        # скорость и направление ветра, м/с
+        self._wind = [
+            w.text for w in s.find_all('div', 'wind_value js_meas_container')
+        ]
+
+        # давление, мм рт. ст.
+        _s = s.find('div', 'pressureline')
+        self._pressure = [
+            p.text for p in _s.find_all('span', 'js_value val_to_convert')
+        ]
+
+        # влажность, %
+        self._humidity = [
+            h.text for h in s.find_all('div', 'humidity_value')
+        ]
+
+        # атмосферные осадки, мм
+        _s = s.find('div', '_line precipitationline js_precipitation clearfix')
+        self._precipitation = [
+            r.text.strip() for r in _s.find_all('div', 'weather_item')
+        ]
+        if self._precipitation == []:
+            self._precipitation = ['0'] * 9
+
+    def get(self):
+        return {
+            'date': time.strftime("%d.%m.%y"),
+            'temperature': self._temperature,
+            'wind': self._wind,
+            'pressure': self._pressure,
+            'humidity': self._humidity,
+            'precipitation': self._precipitation
+        }
