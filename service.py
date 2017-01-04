02ead0eee3a2a6b27c2250a60d690fc392b55ad4
jd <NikeLambert@gmail.com>
2017-01-04 12:50:53 +0300

    Запрос совершается каждый час

--- service.py
+++ service.py
@@ -6,17 +6,20 @@ from mongo import Mongo
 
 __author__ = "Evgeny Goncharov"
 
-w = Weather()
-document = w.get()
-
 m = Mongo()
 
-# запрос содержит текущую дату
-query = {'date': time.strftime("%d.%m.%y")}
+while True:
+    # запрос содержит текущую дату
+    query = {'date': time.strftime("%d.%m.%y")}
 
-if not m.read(query):
-    print('Записи нет в БД')
-    m.write(document=document)
-    print('Документ успешно записан')
-else:
-    print('Запись уже существует')
+    if not m.read(query):
+        print('Записи нет в БД')
+        w = Weather()
+        document = w.get()
+        m.write(document=document)
+        print('Документ успешно записан')
+    else:
+        print('Запись уже существует')
+
+    # делать запрос каждый час
+    time.sleep(3600)
