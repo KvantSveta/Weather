f4136daf765d26d323f278834700f9e5712dd575
jd <NikeLambert@gmail.com>
2017-01-18 12:03:48 +0300

    Сервис запрашивает каждый час погоду и записывает в БД раз в день

--- service.py
+++ service.py
@@ -2,24 +2,41 @@ import time
 
 from weather import Weather
 from mongo import Mongo
+from logger import Logger
 
 
 __author__ = "Evgeny Goncharov"
 
-m = Mongo()
+log = Logger()
+m = Mongo(log)
+
+log.logger.info('Сервис запущен')
 
 while True:
     # запрос содержит текущую дату
     query = {'date': time.strftime("%d.%m.%y")}
 
-    if not m.read(query):
-        print('Записи нет в БД')
-        w = Weather()
-        document = w.get()
-        m.write(document=document)
-        print('Документ успешно записан')
+    # сервер БД доступен
+    if m.ping_mongodb:
+        # записи нет в БД
+        if not m.find_one(query):
+            log.logger.info('Записи нет в БД')
+            w = Weather(log)
+            # информация о погоде успешно получена
+            if w.ok_response:
+                m.insert_one(document=w.get_weather)
+                log.logger.info('Документ успешно записан')
+            else:
+                log.logger.critical('Невозможно получить информацию о погоде')
+        else:
+            log.logger.info('Запись уже существует')
     else:
-        print('Запись уже существует')
+        log.logger.critical('Сервер с БД недоступен')
 
-    # делать запрос каждый час
-    time.sleep(3600)
+    try:
+        # делать запрос каждый час
+        time.sleep(3600)
+    except:
+        break
+
+log.logger.info('Сервис остановлен\n')
