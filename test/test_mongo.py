4683cd121ec2676df0177dd4a481f9798ebfdaa5
jd <NikeLambert@gmail.com>
2017-01-18 10:39:15 +0300

    Добавлен тест для определения наличия подключения и удаление ненужного лог-файла

--- test/test_mongo.py
+++ test/test_mongo.py
@@ -1,5 +1,6 @@
-import unittest2 as unittest
 import time
+import subprocess
+import unittest2 as unittest
 
 from mongo import Mongo
 from logger import Logger
@@ -11,13 +12,11 @@ __author__ = "Evgeny Goncharov"
 class TestMongo(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        file_name = 'test_mongo.log'
-        self.mongo_local = Mongo(
-            Logger(file_name=file_name), ip_address='127.0.0.1', port=27017
-        )
-        self.mongo_host = Mongo(
-            Logger(file_name=file_name), ip_address='192.168.1.102', port=27017
-        )
+        self.file_name = 'test_mongo.log'
+        self.mongo_local = Mongo(Logger(file_name=self.file_name),
+                                 ip_address='127.0.0.1', port=27017)
+        self.mongo_host = Mongo(Logger(file_name=self.file_name),
+                                ip_address='192.168.1.102', port=27017)
 
     def test_find(self):
         answer = self.mongo_local.find()
@@ -76,10 +75,14 @@ class TestMongo(unittest.TestCase):
         new_count = self.mongo_local._collection_day.count()
         self.assertEqual(new_count, count)
 
+    def test_connect(self):
+        self.assertTrue(self.mongo_local.ping_mongodb)
+
+        self.assertFalse(self.mongo_host.ping_mongodb)
+
     @classmethod
     def tearDownClass(self):
-        del self.mongo_host
-        del self.mongo_local
+        subprocess.call(['rm', self.file_name])
 
 
 if __name__ == '__main__':
