548db18d8b94a388735173d00beda289d54e9517
jd <NikeLambert@gmail.com>
2017-01-21 14:46:55 +0300

    Мелкий рефакторинг

--- logger.py
+++ logger.py
@@ -5,10 +5,10 @@ __author__ = "Evgeny Goncharov"
 
 
 class Logger():
-    def __init__(self, file_name='weather.log'):
-        file_handler = logging.FileHandler(file_name, 'a')
+    def __init__(self, file_name="weather.log"):
+        file_handler = logging.FileHandler(file_name, "a")
         formatter = logging.Formatter(
-            fmt='%(asctime)s %(levelname)s %(message)s',
+            fmt="%(asctime)s %(levelname)s %(message)s",
             datefmt="%e %b %y %H:%M:%S"
         )
         file_handler.setFormatter(fmt=formatter)
