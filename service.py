1de274d733bcbf4d65636525387b74de5f2bbf95
jd <NikeLambert@gmail.com>
2017-01-21 14:48:40 +0300

    Мелкий рефакторинг

--- service.py
+++ service.py
@@ -19,37 +19,37 @@ m = Mongo(log)
 
 def handler(signum, frame):
     run_service.clear()
-    log.logger.info('Сигнал для остановки контейнера (%s)', signum)
+    log.logger.info("Сигнал для остановки контейнера (%s)", signum)
 
 
 signal.signal(signal.SIGTERM, handler)
 
-log.logger.info('Сервис запущен')
+log.logger.info("Сервис запущен")
 
 while run_service.is_set():
     # запрос содержит текущую дату
-    query = {'date': time.strftime("%d.%m.%y")}
+    query = {"date": time.strftime("%d.%m.%y")}
 
     # сервер БД доступен
     if m.ping_mongodb:
         # записи нет в БД
         if not m.find_one(query):
-            log.logger.info('Записи нет в БД')
+            log.logger.info("Записи нет в БД")
             w = Weather(log)
             # информация о погоде успешно получена
             if w.ok_response:
                 m.insert_one(document=w.get_weather)
-                log.logger.info('Документ успешно записан')
+                log.logger.info("Документ успешно записан")
             else:
-                log.logger.critical('Невозможно получить информацию о погоде')
+                log.logger.critical("Невозможно получить информацию о погоде")
         else:
-            log.logger.info('Запись уже существует')
+            log.logger.info("Запись уже существует")
     else:
-        log.logger.critical('Сервер с БД недоступен')
+        log.logger.critical("Сервер с БД недоступен")
 
     start = time.time()
     # делать запрос каждый час
     while run_service.is_set() and time.time() - start < 3600:
         time.sleep(1)
 
-log.logger.info('Сервис остановлен\n')
+log.logger.info("Сервис остановлен\n")
