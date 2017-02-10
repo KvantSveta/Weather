445d92d7f4e040c270703b044f65ea43fb573d76
jd <NikeLambert@gmail.com>
2017-02-10 15:15:17 +0300

    Добавлен маршрут для отображения температуры малинки
    Добавлен html и css файлы для описания разметки и стиля

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-from flask import Flask
+import time
+from subprocess import check_output
 
+from flask import Flask, render_template
+
+from mongo import Mongo
+from logger import Logger
 
 __author__ = "Evgeny Goncharov"
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="")
+
+log = Logger("web.log")
+m = Mongo(log)
 
 
-@app.route('/')
-def hello_world():
-    return "Hello, World!"
+@app.route("/", methods=["GET"])
+def temperature():
+    temp = check_output(["cat", "/sys/class/thermal/thermal_zone0/temp"])
+    temp = temp.decode()
+    temp = str(round(int(temp) / 1000, 1)) + "'C"
+    current_time = str(time.strftime("%H:%M:%S - %d.%m.%y"))
+    return render_template(
+        "temperature.html", temperature=temp, current_time=current_time
+    )
 
 
-app.run(host="192.168.1.102", port=5000, debug=True)
+@app.route("/index", methods=["GET"])
+def index():
+    query = {"date": time.strftime("%d.%m.%y")}
+    current_time = str(time.strftime("%H:%M:%S - %d.%m.%y"))
+    return current_time + "<br>" + str(m.find_one(query))
+
+
+app.run(host="localhost", port=5000, debug=True)
