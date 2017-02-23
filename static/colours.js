2fcd9c580a87074b02faa48937fb8e92b1d299e2
jd <NikeLambert@gmail.com>
2017-02-23 14:47:54 +0300

    Создан js для получения цвета и отправки его на сервер

---
+++ static/colours.js
@@ -0,0 +1,35 @@
+
+function GetColours() {
+    var red_colour = document.getElementById("red").value;
+    var green_colour = document.getElementById("green").value;
+    var blue_colour = document.getElementById("blue").value;
+
+    red_colour = parseInt(red_colour);
+    green_colour = parseInt(green_colour);
+    blue_colour = parseInt(blue_colour);
+
+    if (!Number.isInteger(red_colour) || 0 > red_colour || red_colour > 255) {
+        red_colour = 0;
+    }
+
+    if (!Number.isInteger(green_colour) || 0 > green_colour || green_colour > 255) {
+        green_colour = 0;
+    }
+
+    if (!Number.isInteger(blue_colour) || 0 > blue_colour || blue_colour > 255) {
+        blue_colour = 0;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/colours", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(
+        JSON.stringify(
+            {
+                "red": red_colour,
+                "green": green_colour,
+                "blue": blue_colour
+            }
+        )
+    )
+}
