3f17fc978c0935a45ab67c703fcabdbd186cdbd6
jd <NikeLambert@gmail.com>
2017-02-23 15:17:22 +0300

    Модуль реализующий ШИМ для led

---
+++ pwm.py
@@ -0,0 +1,51 @@
+import time
+import threading
+try:
+    import RPi.GPIO as GPIO
+    import_package = True
+except RuntimeError:
+    import_package = False
+
+__author__ = "Evgeny Goncharov"
+
+
+class Pwm_led():
+    def __init__(self):
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+
+        self._red_gpio = 17
+        self._green_gpio = 27
+        self._blue_gpio = 22
+
+        GPIO.setup(self._red_gpio, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self._green_gpio, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self._blue_gpio, GPIO.OUT, initial=GPIO.LOW)
+
+        self._red_frq = GPIO.PWM(self._red_gpio, 50)
+        self._red_frq.start(0)
+        self._green_frq = GPIO.PWM(self._green_gpio, 50)
+        self._green_frq.start(0)
+        self._blue_frq = GPIO.PWM(self._blue_gpio, 50)
+        self._blue_frq.start(0)
+
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            threading.Thread(target=func, args=args, kwargs=kwargs).start()
+
+        return wrapper
+
+    @decorator
+    def change_colour(self, time_shine=60, red=0, green=0, blue=0):
+        self._red_frq.ChangeDutyCycle(red)
+        self._green_frq.ChangeDutyCycle(green)
+        self._blue_frq.ChangeDutyCycle(blue)
+
+        time.sleep(time_shine)
+
+        self._red_frq.stop()
+        self._green_frq.stop()
+        self._blue_frq.stop()
+
+    def __del__(self):
+        GPIO.cleanup([self._red_gpio, self._green_gpio, self._blue_gpio])
