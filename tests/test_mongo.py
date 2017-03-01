5f0e0a719d4d2624a63e8e47d0942368315336c9
jd <NikeLambert@gmail.com>
2017-03-01 10:38:48 +0300

    Добавлена возможность записи в БД из теста

--- tests/test_mongo.py
+++ tests/test_mongo.py
@@ -5,6 +5,7 @@ import unittest2 as unittest
 
 from main.logger import Logger
 from main.mongo import Mongo
+from main.weather import Weather
 
 __author__ = "Evgeny Goncharov"
 
@@ -12,21 +13,28 @@ __author__ = "Evgeny Goncharov"
 class TestMongo(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.file_name = 'test_mongo.log'
-        self.mongo_local = Mongo(Logger(file_name=self.file_name),
-                                 ip_address='127.0.0.1', port=27017)
-        self.mongo_host = Mongo(Logger(file_name=self.file_name),
-                                ip_address='192.168.1.102', port=27017)
+        self.mongo_log = "test_mongo.log"
+        self.weather_log = "test_weather.log"
+        self.mongo_local = Mongo(Logger(file_name=self.mongo_log),
+                                 ip_address="127.0.0.1", port=27017)
+
+        if self.mongo_local.ping_mongodb:
+            w = Weather(Logger(file_name=self.weather_log))
+            if w.ok_response:
+                self.mongo_local.insert_one(document=w.get_weather)
+
+        self.mongo_host = Mongo(Logger(file_name=self.mongo_log),
+                                ip_address="192.168.1.102", port=27017)
 
     def test_find(self):
         answer = self.mongo_local.find()
         self.assertTrue(answer)
 
-        query = {'date': time.strftime("%d.%m.%y")}
+        query = {"date": time.strftime("%d.%m.%y")}
         answer = self.mongo_local.find(query=query)
         self.assertTrue(answer)
 
-        _filter = {'temperature': 1}
+        _filter = {"temperature": 1}
         answer = self.mongo_local.find(query=query, _filter=_filter)
         self.assertTrue(answer)
 
@@ -43,11 +51,11 @@ class TestMongo(unittest.TestCase):
         answer = self.mongo_local.find()
         self.assertTrue(answer)
 
-        query = {'date': time.strftime("%d.%m.%y")}
+        query = {"date": time.strftime("%d.%m.%y")}
         answer = self.mongo_local.find(query=query)
         self.assertTrue(answer)
 
-        _filter = {'temperature': 1}
+        _filter = {"temperature": 1}
         answer = self.mongo_local.find(query=query, _filter=_filter)
         self.assertTrue(answer)
 
@@ -62,7 +70,7 @@ class TestMongo(unittest.TestCase):
 
     def test_insert(self):
         count = self.mongo_local._collection_day.count()
-        document = {'temp': 123456}
+        document = {"temp": 123456}
         self.mongo_local.insert_one(document=document)
         new_count = self.mongo_local._collection_day.count()
         self.assertEqual(new_count, count + 1)
@@ -82,8 +90,8 @@ class TestMongo(unittest.TestCase):
 
     @classmethod
     def tearDownClass(self):
-        subprocess.call(['rm', self.file_name])
+        subprocess.call(["rm", self.mongo_log, self.weather_log])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
--- tests/test_weather.py
+++ tests/test_weather.py
@@ -11,23 +11,26 @@ __author__ = "Evgeny Goncharov"
 class TestWeather(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.file_name = 'test_weather.log'
+        self.weather_log = "test_weather.log"
 
     def test_good_connect(self):
-        w = Weather(Logger(file_name=self.file_name))
+        w = Weather(Logger(file_name=self.weather_log))
         self.assertTrue(w.ok_response)
         self.assertTrue(w.get_weather)
 
     def test_bad_connect(self):
-        w = Weather(Logger(file_name=self.file_name), page='https:/10.0.0.1/')
+        w = Weather(
+            Logger(file_name=self.weather_log),
+            page="https:/10.0.0.1/"
+        )
         self.assertFalse(w.ok_response)
         with self.assertRaises(AttributeError):
             w.get_weather
 
     @classmethod
     def tearDownClass(self):
-        subprocess.call(['rm', self.file_name])
+        subprocess.call(["rm", self.weather_log])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
