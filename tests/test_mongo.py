eb0f12353af2554e2902c80fde68e545d74ad2d4
jd <NikeLambert@gmail.com>
2017-03-03 11:14:24 +0300

    Исправлен баг и добавлена проверка на наличее БД

--- tests/test_mongo.py
+++ tests/test_mongo.py
@@ -9,23 +9,32 @@ from main.weather import Weather
 
 __author__ = "Evgeny Goncharov"
 
+mongo_available = Mongo(
+    Logger(file_name="test_mongo.log"),
+    ip_address="127.0.0.1",
+    port=27017
+).ping_mongodb
+
 
 class TestMongo(unittest.TestCase):
     @classmethod
-    def setUpClass(self):
-        self.mongo_log = "test_mongo.log"
-        self.weather_log = "test_weather.log"
-        self.mongo_local = Mongo(Logger(file_name=self.mongo_log),
-                                 ip_address="127.0.0.1", port=27017)
+    def setUpClass(cls):
+        cls.mongo_log = "test_mongo.log"
+        cls.weather_log = "test_weather.log"
+        cls.mongo_local = Mongo(Logger(file_name=cls.mongo_log),
+                                ip_address="127.0.0.1",
+                                port=27017)
 
-        if self.mongo_local.ping_mongodb:
-            w = Weather(Logger(file_name=self.weather_log))
+        if cls.mongo_local.ping_mongodb:
+            w = Weather(Logger(file_name=cls.weather_log))
             if w.ok_response:
-                self.mongo_local.insert_one(document=w.get_weather)
+                cls.mongo_local.insert_one(document=w.get_weather)
 
-        self.mongo_host = Mongo(Logger(file_name=self.mongo_log),
-                                ip_address="192.168.1.102", port=27017)
+        cls.mongo_host = Mongo(Logger(file_name=cls.mongo_log),
+                               ip_address="192.168.1.102",
+                               port=27017)
 
+    @unittest.skipUnless(mongo_available, "Mongo DB not available")
     def test_find(self):
         answer = self.mongo_local.find()
         self.assertTrue(answer)
@@ -47,6 +56,7 @@ class TestMongo(unittest.TestCase):
         answer = self.mongo_host.find(query=query, _filter=_filter)
         self.assertFalse(answer)
 
+    @unittest.skipUnless(mongo_available, "Mongo DB not available")
     def test_find_one(self):
         answer = self.mongo_local.find()
         self.assertTrue(answer)
@@ -68,6 +78,7 @@ class TestMongo(unittest.TestCase):
         answer = self.mongo_host.find(query=query, _filter=_filter)
         self.assertFalse(answer)
 
+    @unittest.skipUnless(mongo_available, "Mongo DB not available")
     def test_insert(self):
         count = self.mongo_local._collection_day.count()
         document = {"temp": 123456}
@@ -83,14 +94,18 @@ class TestMongo(unittest.TestCase):
         new_count = self.mongo_local._collection_day.count()
         self.assertEqual(new_count, count)
 
+    @unittest.skipUnless(mongo_available, "Mongo DB not available")
     def test_connect(self):
         self.assertTrue(self.mongo_local.ping_mongodb)
 
         self.assertFalse(self.mongo_host.ping_mongodb)
 
     @classmethod
-    def tearDownClass(self):
-        subprocess.call(["rm", self.mongo_log, self.weather_log])
+    def tearDownClass(cls):
+        subprocess.call(["rm", cls.mongo_log])
+
+        if mongo_available:
+            subprocess.call(["rm", cls.weather_log])
 
 
 if __name__ == "__main__":
