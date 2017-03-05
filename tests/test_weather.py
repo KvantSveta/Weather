7bad5a5cdd77bed9b3ec42c1dfc3eb647c30a686
jd <NikeLambert@gmail.com>
2017-03-05 19:48:24 +0300

    Добавлены тесты

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -15,7 +15,21 @@ class TestWeather(unittest.TestCase):
 
     def test_good_connect(self):
         w = Weather(Logger(file_name=self.weather_log))
+
         self.assertTrue(w.ok_response)
+
+        self.assertTrue(w.date)
+
+        self.assertTrue(w.temperature)
+
+        self.assertTrue(w.wind)
+
+        self.assertTrue(w.pressure)
+
+        self.assertTrue(w.humidity)
+
+        self.assertTrue(w.precipitation)
+
         self.assertTrue(w.get_weather)
 
     def test_bad_connect(self):
@@ -23,7 +37,27 @@ class TestWeather(unittest.TestCase):
             Logger(file_name=self.weather_log),
             page="https:/10.0.0.1/"
         )
+
         self.assertFalse(w.ok_response)
+
+        with self.assertRaises(AttributeError):
+            w.date
+
+        with self.assertRaises(AttributeError):
+            w.temperature
+
+        with self.assertRaises(AttributeError):
+            w.wind
+
+        with self.assertRaises(AttributeError):
+            w.pressure
+
+        with self.assertRaises(AttributeError):
+            w.humidity
+
+        with self.assertRaises(AttributeError):
+            w.precipitation
+
         with self.assertRaises(AttributeError):
             w.get_weather
 
